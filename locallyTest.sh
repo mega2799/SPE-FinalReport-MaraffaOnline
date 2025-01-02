04219441f9c78b5ced3405e33a1cab86ff1f6545
『 Matteo Santoro 』 <65976958+mega2799@users.noreply.github.com>
2025-01-02 17:56:25 +0100

    Update locallyTest.sh

--- locallyTest.sh
+++ locallyTest.sh
@@ -1,6 +1,8 @@
 #!/usr/env/bin bash
 
 #pandoc  -s --highlight-style kate --toc -c latex2html.css  report/main.tex  -o index.html
-pandoc -s --highlight-style kate --toc --self-contained -c enhanced.css report/main.tex -o index.html
+curl 'https://raw.githubusercontent.com/ryangrose/easy-pandoc-templates/master/copy_templates.sh' | bash
+pandoc  -s --highlight-style kate --toc --template easy_template.html report/main.tex --metadata-file=metadata.yaml  -o index.html
+#pandoc -s --highlight-style kate --toc --self-contained -c enhanced.css report/main.tex -o index.html
 
 latexmk -pdf report/main.tex
