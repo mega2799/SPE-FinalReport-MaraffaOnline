cc1c7c2584f11d328bca6bb9a582c5ecc9f0ccdf
『 Matteo Santoro 』 <65976958+mega2799@users.noreply.github.com>
2025-01-02 16:55:46 +0100

    Update locallyTest.sh

--- locallyTest.sh
+++ locallyTest.sh
@@ -1,4 +1,6 @@
 #!/usr/env/bin bash
 
-pandoc  -s --highlight-style kate --toc -c latex2html.css  report/main.tex  -o index.html
+#pandoc  -s --highlight-style kate --toc -c latex2html.css  report/main.tex  -o index.html
+pandoc -s --highlight-style kate --toc --self-contained -c enhanced.css report/main.tex -o index.html
+
 latexmk -pdf report/main.tex
